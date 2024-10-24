be2ef3107ab2a7ff44ddf93321f3555b7014e9f7
Manh Nguyen <manhnv5@vng.com.vn>
2024-10-24 21:21:15 +0700

    init go

---
+++ module1/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const PI = 3.14
+
+func main() {
+	var age int = 28
+	var amount int64 = 1000
+	fmt.Println(age)
+	fmt.Println(amount)
+	name := "Nguyen A"
+	fmt.Println(name)
+	fmt.Println("Hello word")
+	fmt.Println(PI)
+}
